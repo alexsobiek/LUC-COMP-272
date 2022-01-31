5d50d96656f8323313921e760afe5f206dac65ab
Alex Sobiek <38387876+alexsobiek@users.noreply.github.com>
2022-01-30 21:58:29 -0600

    Move to java folder

---
+++ java/src/main/java/com/alexsobiek/luc/comp272/queues/QueueExercise.java
@@ -0,0 +1,14 @@
+package com.alexsobiek.luc.comp272.queues;
+
+import java.util.LinkedList;
+import java.util.Queue;
+
+public class QueueExercise {
+    public static void main(String[] args) {
+        new QueueExercise(); // Escape Static
+    }
+
+    public QueueExercise() {
+        Queue<Integer> intQueue = new LinkedList<>();
+    }
+}
